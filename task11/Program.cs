c1989f9af35e76ea9345b7e7b9620f4498e376c7
evilfoxes <evilfoxes@mail.ru>
2023-08-11 15:19:17 +1100

    Task_11_2

--- task11/Program.cs
+++ task11/Program.cs
@@ -8,10 +8,10 @@
 int number = new Random().Next(100, 1000);
 Console.WriteLine($"Случайное число из диапозона 100-999 -> {number}");
 
-int firstDigit = number / 100;
-int lastDigit = number % 10;
-int result = firstDigit * 10 + lastDigit;
-Console.WriteLine(result);
+// int firstDigit = number / 100;
+// int lastDigit = number % 10;
+// int result = firstDigit * 10 + lastDigit;
+// Console.WriteLine(result);
 
 int twoNumbers = TwoNumbers(number);
 Console.WriteLine(twoNumbers);
