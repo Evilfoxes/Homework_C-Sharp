011a0dc871c6bef6692035632faa0a081831cfc8
evilfoxes <evilfoxes@mail.ru>
2023-08-11 16:30:11 +1100

    Task14

---
+++ task14/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход
+// число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+
+// 14 -> Нет 
+// 46 -> Нет
+// 161 -> Да 
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = MultiplicityTwoDigits(number, 7 ,23);
+Console.WriteLine(result ? "Да" : "Нет");
+
+bool MultiplicityTwoDigits(int number1, int number2, int number3)
+{
+    return number1 % number2 == 0 && number1 % number3 == 0;
+}
