c0fb6418c67fe69ff0b04215e15a25c8dd237026
evilfoxes <evilfoxes@mail.ru>
2023-08-24 18:07:52 +1100

    Fixed_bug_in_task12

--- task12/Program.cs
+++ task12/Program.cs
@@ -11,10 +11,10 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-int remainder = Remainder(num1, num2);
+int remainder = Remainder(number1, number2);
 
-if (remainder == 0) Console.WriteLine("Число: " + number1 + " кратное числу: " + number2 );
-else Console.WriteLine("Число: " + number1 + " не кратное числу: " + number2 + " остаток от деления: " + remainder);
+if (remainder == 0) Console.WriteLine($"Число: " + number1 + " кратное числу: " + number2);
+else Console.WriteLine($"Число: " + number1 + " не кратное числу: " + number2 + " остаток от деления: " + remainder);
 
 
 int Remainder(int num1, int num2)
