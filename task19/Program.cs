faf24f8c87c3f5c0850da0bd05b6d089ee399e18
evilfoxes <evilfoxes@mail.ru>
2023-08-24 18:34:49 +1100

    Create_Task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -6,3 +6,15 @@
 // 23432 -> да
 // 12821 -> да
 
+Console.Write("Enter a five-digit number: ");
+string number = Console.ReadLine();
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.Write($"{number} - YES, it is palindrome");
+    }
+    else
+    {
+        Console.Write($"{number} - NO, this number is not a palindrome");
+    }
+}
