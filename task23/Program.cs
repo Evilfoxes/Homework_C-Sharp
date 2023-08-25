7d008418aa2921b79a79fc1d5fce0ba952bff22f
evilfoxes <evilfoxes@mail.ru>
2023-08-25 12:05:46 +1100

    Changes_made

--- task23/Program.cs
+++ task23/Program.cs
@@ -9,7 +9,7 @@ void TableSquare(int num)
     int count = 1;
     while(count <= num)
     {
-        Console.WriteLine($"{count, 3} -> {count * count * count, 5}");
+        Console.WriteLine($"{count, 3} | {count * count * count, 5}");
         count++;
     }
 }
