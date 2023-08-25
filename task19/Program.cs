204ea7970954bd895b2da11667dad7fa5103eb89
evilfoxes <evilfoxes@mail.ru>
2023-08-25 11:47:51 +1100

    Fixes_bugs_task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -6,15 +6,26 @@
 // 23432 -> да
 // 12821 -> да
 
-Console.Write("Enter a five-digit number: ");
-string number = Console.ReadLine();
+internal class Program
 {
-    if (number[0] == number[4] && number[1] == number[3])
+    private static void Main(string[] args)
     {
-        Console.Write($"{number} - YES, it is palindrome");
-    }
-    else
-    {
-        Console.Write($"{number} - NO, this number is not a palindrome");
+        Console.Write("Enter a five-digit number: ");
+        string number = Console.ReadLine();
+    
+
+        void CheckingNumber(string number)
+        {
+            if (number[0] == number[4] && number[1] == number[3])
+            {
+                Console.Write($"{number} - YES, it is palindrome");
+            }
+            else Console.Write($"{number} - NO, this number is not a palindrome");
+        }
+        if (number!.Length == 5)
+        {
+            CheckingNumber(number);
+        }
+        else Console.WriteLine("Error, wrong number");
     }
 }
