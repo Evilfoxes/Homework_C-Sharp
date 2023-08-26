d2808249184f0f2e6b4f470ba1442fa7ecd02682
evilfoxes <evilfoxes@mail.ru>
2023-08-27 09:03:32 +1100

    Change6

--- task19/Program.cs
+++ task19/Program.cs
@@ -6,46 +6,6 @@
 // 23432 -> да
 // 12821 -> да
 
-// int FiveDigitNumber(string massage)
-// {
-// 	Console.Write(massage);
-// 	int number = Convert.ToInt32(Console.ReadLine());
-// 	return number;
-// }
-
-// int Reverse(int number) //метод для переворачивания числа
-// { 
-//     int reverse = 0;
-//     if (number > 10000 && number < 99999)
-//     {
-//         int value = number % 10;
-//         reverse = reverse * 10 + value;
-//         number = number / 10;
-//     }
-
-//     else    
-//     {
-//         Console.WriteLine ("Errore, wrong number");
-//     }
-
-// return reverse; // возвращает перевернутое число
-// }
-
-// bool Palindrome(int number) // метод для определения палиндрома
-// {
-//     return number == Reverse(number);
-// }
-
-// int number = FiveDigitNumber("Enter five-digit number: ");
-// if (Palindrome(number))
-// {
-//     Console.WriteLine ($"Yes {number} it is palindrome");
-// }
-// else
-// {
-//     Console.WriteLine ($"No {number} this number is not a palindrome");
-// }
-
 Console.Write("Enter five-digit number: ");
 int number = int.Parse(Console.ReadLine());
 int RevNumber = 0;
