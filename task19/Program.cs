1a027c60e88e5cbb5122280c969b7fd21633e1a6
evilfoxes <evilfoxes@mail.ru>
2023-08-26 19:11:04 +1100

    Change2

--- task19/Program.cs
+++ task19/Program.cs
@@ -6,57 +6,24 @@
 // 23432 -> да
 // 12821 -> да
 
-// Решение задачи через строку
-
-// internal class Program
-// {
-//     private static void Main(string[] args)
-//     {
-//         Console.Write("Enter a five-digit number: ");
-//         string number = Console.ReadLine();
-    
-//         void CheckingNumber(string number)
-//         {
-//             if (number[0] == number[4] && number[1] == number[3])
-//             {
-//                 Console.Write($"{number} - YES, it is palindrome");
-//             }
-//             else Console.Write($"{number} - NO, this number is not a palindrome");
-//         }
-//         if (number!.Length == 5)
-//         {
-//             CheckingNumber(number);
-//         }
-//         else Console.WriteLine("Error, wrong number");
-//     }
-// }
-
 int ReadData(string msg)
 {
     Console.WriteLine(msg);
     return int.Parse(Console.ReadLine() ?? "0");
 }
 
-//Метод выводит результат пользователю
-void PrintData(string msg)
+void PrintData(string msg) //Метод выводит результат пользователю
 {
     Console.WriteLine(msg);
 }
 
-// Метод заполнения словаря FillDictionary
-
-string FillDictionary(int num)
+string FillDictionary(int num) // Метод заполнения словаря FillDictionary
 {
-    //Объявляем переменную res и присваиваем ей значение пull
-    string res = null;
-
-    // Начальное значение счетчика. Левая граница диапазона всех 5-ти значных чисел
-    int count = 10000;
-    // Иницилизация пустого словаря с именем allPalindr
-    Dictionary<int, string> allPalindr = new Dictionary<int, string>();
+    string res = null; //Объявляем переменную res и присваиваем ей значение пull
+    int count = 10000; // Начальное значение счетчика. Левая граница диапазона всех 5-ти значных чисел
+    Dictionary<int, string> allPalindr = new Dictionary<int, string>(); // Иницилизация пустого словаря с именем allPalindr
     
-    //Перебор всех 5-ти значных чисел. 99999 - это права граница диапазона всех 5-ти значных чисел
-    while (count <= 99999)
+    while (count <= 99999) //Перебор всех 5-ти значных чисел. 99999 - это права граница диапазона всех 5-ти значных чисел
     {
         // Парсим первые 2 и последние 2 цифры  числа
         int d1 = count / 10000;
@@ -67,10 +34,8 @@ string FillDictionary(int num)
         if ((d1 == d4) && (d2 == d3))
         {
             // создаем строковую переменную value и записываем в нее число "d1d2d3d4"
-            // ToString конвертирует из int в string
-            string value = "YES, it is palindrome ";
-            // добавляем новый эл-т массива allPalindr с ключом - count и значением - value
-            allPalindr.Add(count, value);
+            string value = "YES, it is palindrome "; // ToString конвертирует из int в string
+            allPalindr.Add(count, value); // добавляем новый эл-т массива allPalindr с ключом - count и значением - value
         }
         else
         {
@@ -80,13 +45,6 @@ string FillDictionary(int num)
         count = count + 1;
 
     }
-
-
-    // // только для проверки вывод в консоль словаря
-    // foreach (var item in allPalindr)
-    // {
-    //     Console.WriteLine($"Ключ: {item.Key}  Значение: {item.Value}");
-    // }
 
     res = allPalindr[num];
     return res;
