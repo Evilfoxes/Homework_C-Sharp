867dcafe4fddd34a4f1f5ae0dec8c6e24afa73bb
evilfoxes <evilfoxes@mail.ru>
2023-08-27 09:00:34 +1100

    Change6

--- task19/Program.cs
+++ task19/Program.cs
@@ -6,42 +6,69 @@
 // 23432 -> да
 // 12821 -> да
 
-int FiveDigitNumber(string massage)
-{
-	Console.Write(massage);
-	int number = Convert.ToInt32(Console.ReadLine());
-	return number;
-}
+// int FiveDigitNumber(string massage)
+// {
+// 	Console.Write(massage);
+// 	int number = Convert.ToInt32(Console.ReadLine());
+// 	return number;
+// }
 
-int Reverse(int number) //метод для переворачивания числа
-{ 
-    int reverse = 0;
-    if (number >= 0 && number <= 99999)
+// int Reverse(int number) //метод для переворачивания числа
+// { 
+//     int reverse = 0;
+//     if (number > 10000 && number < 99999)
+//     {
+//         int value = number % 10;
+//         reverse = reverse * 10 + value;
+//         number = number / 10;
+//     }
+
+//     else    
+//     {
+//         Console.WriteLine ("Errore, wrong number");
+//     }
+
+// return reverse; // возвращает перевернутое число
+// }
+
+// bool Palindrome(int number) // метод для определения палиндрома
+// {
+//     return number == Reverse(number);
+// }
+
+// int number = FiveDigitNumber("Enter five-digit number: ");
+// if (Palindrome(number))
+// {
+//     Console.WriteLine ($"Yes {number} it is palindrome");
+// }
+// else
+// {
+//     Console.WriteLine ($"No {number} this number is not a palindrome");
+// }
+
+Console.Write("Enter five-digit number: ");
+int number = int.Parse(Console.ReadLine());
+int RevNumber = 0;
+int modNumber = number;
+
+if (number > 10000 && number <99999)
+{
+    while (modNumber > 0)
     {
-        int value = number % 10;
-        reverse = reverse * 10 + value;
-        number = number / 10;
+        RevNumber = (RevNumber * 10) + (modNumber % 10);
+        modNumber = modNumber / 10;
     }
-    else 
+
+    if (RevNumber == number)
     {
-        Console.WriteLine("Error, wrong number");
+        Console.WriteLine ($"YES {number} it's palindrome");
     }
-return reverse; // возвращает перевернутое число
-}
-
-bool Palindrome(int number) // метод для определения палиндрома
-{
-    return number == Reverse(number);
-}
-
-int number = FiveDigitNumber("Enter five-digit number: ");
-if (Palindrome(number))
-{
-    Console.WriteLine ($"Yes {number} it is palindrome");
+    else
+    {
+        Console.WriteLine ($"NO {number} this number is not a pollindrome");
+    }
 }
 else
 {
-    Console.WriteLine ($"No {number} this number is not a palindrome");
+    Console.WriteLine($"{number} wrong number!!!");
 }
-
-        
