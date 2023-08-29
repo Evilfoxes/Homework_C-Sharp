a063d7fc6555b0207d646aa63102edc734b4778f
evilfoxes <evilfoxes@mail.ru>
2023-08-29 12:49:39 +1100

    Save_Task28

---
+++ task28/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая 
+// прнимает на вход число N и выдаёт 
+// произведение чисел от 1 до N.
+
+// 4 -> 24
+// 5 -> 120
+
+int ProdNumber(int num)
+{
+    int prod = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            prod *= i;   
+        }
+    }
+    return prod;
+}
+
+Console.Write("Enter natural number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = ProdNumber(number);
+Console.WriteLine($"Product of number from 1 to {number} = {result}");
