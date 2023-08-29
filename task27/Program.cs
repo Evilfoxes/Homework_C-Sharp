9e22a559e6451910e9b475918411fed39555a9c1
evilfoxes <evilfoxes@mail.ru>
2023-08-29 16:07:36 +1100

    SaveTheTask27_01

--- task27/Program.cs
+++ task27/Program.cs
@@ -5,3 +5,21 @@
 // 82 -> 10
 // 9012 -> 12
 
+
+
+Console.Write("Enter natural number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int number)
+{
+    int result = 0;
+    while (0 < number)
+    {
+        result += number % 10;
+        number /= 10;
+    }
+    return result;
+}
+
+int result = SumDigits(number);
+Console.WriteLine($"{number, 5} -> {result, 3}");
