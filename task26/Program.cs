6395f49071dcc202790407417a55302d25adbe13
evilfoxes <evilfoxes@mail.ru>
2023-08-29 12:11:52 +1100

    Save_Task26

---
+++ task26/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+int NumDigits (int num)
+{
+    int sum = 0;
+    int count = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.Write("Enter natural number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = NumDigits(number);
+Console.WriteLine($"Number of digits = {result}");
