af781e1e564bb34f56e8b5f38ee1ff38fb311740
evilfoxes <evilfoxes@mail.ru>
2023-08-29 12:17:29 +1100

    Save_Task26

--- task26/Program.cs
+++ task26/Program.cs
@@ -4,8 +4,9 @@
 // 89126 -> 5
 int NumDigits (int num)
 {
+    if (num == 0) return 1;
     int count = 0;
-    while (num > 0)
+    while (num != 0)
     {
         num = num / 10;
         count++;
