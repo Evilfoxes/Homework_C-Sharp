301de9cf4f18b8a497ac23ca2eec4d72a6ff7c8f
evilfoxes <evilfoxes@mail.ru>
2023-08-29 15:45:43 +1100

    SaveTheTask25_01

--- task25/Program.cs
+++ task25/Program.cs
@@ -4,3 +4,15 @@
 
 // 3, 5 -> 243
 // 2, 4 -> 16
+
+Console.Write("Enter natural number (A): ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter natural number (B): ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int deg = 1;
+for (int i = 0; i < B; i++)
+{
+    deg = deg * A;
+}
+Console.WriteLine($"{A} raised to a natural degree {B} = {deg}");
