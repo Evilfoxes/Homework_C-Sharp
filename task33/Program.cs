94e2e993c1f9a6e50ed397fa2ecb239bb0d53077
evilfoxes <evilfoxes@mail.ru>
2023-09-01 22:24:25 +1100

    Save_md_Task33

--- task33/Program.cs
+++ task33/Program.cs
@@ -3,3 +3,43 @@
 
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]},");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+bool SearchNum(int[] arr, int num)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] == num) return true;
+    }
+    return false;
+}
+
+int[] newarr = CreateArrayRndInt (5, -5, 9);
+PrintArray (newarr);
+
+Console.Write("Enter search number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool searchNum = SearchNum(newarr, number);
+Console.WriteLine(searchNum ? "Number present" : "Number missing");
