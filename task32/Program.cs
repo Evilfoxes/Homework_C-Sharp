ec1c3060910d561a71bf7857a03802ca271eced5
evilfoxes <evilfoxes@mail.ru>
2023-09-01 21:40:50 +1100

    Save_md_Task32_01

--- task32/Program.cs
+++ task32/Program.cs
@@ -4,3 +4,38 @@
 
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
 
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void InversionArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]},");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] newarr = CreateArrayRndInt (5, -5, 9);
+PrintArray (newarr);
+Console.Write(" -> ");
+InversionArray(newarr);
+PrintArray(newarr);
