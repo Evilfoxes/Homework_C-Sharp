63e5c783a32dd793a14c839daef87f6b2dc7ab9c
evilfoxes <evilfoxes@mail.ru>
2023-09-05 19:34:54 +1100

    Save_Task38

--- task38/Program.cs
+++ task38/Program.cs
@@ -4,10 +4,7 @@
 
 //  [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
 
-int size = 0;
-int array = 0;
-
-double[] CreatearrayayRndDouble(int size, int min, int max)
+double[] CreateArrayRndDouble(int size, int min, int max)
 {
     double[] array = new double[size];
     Random rnd = new Random();
@@ -18,33 +15,31 @@ double[] CreatearrayayRndDouble(int size, int min, int max)
     return array;
 }
 
-void PrintarrayayDouble(double[] array)
+double FindMaxMin(double[] array)
+{
+    double min = array[0];
+    double max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
+        if (array[i] > max) max = array[i];
+    }
+    double diff = max - min;
+    return diff;
+}
+
+void PrintArrayDouble(double[] array)
 {
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        if (i < array.Length - 1) Console.Write($"{array[i]:F1}, ");
         else Console.Write($"{array[i]:F1}");
     }
     Console.WriteLine("]");
 }
 
-void FindMaxMin(int[] array)
-{
-    int min = array[0];
-    int max = array[0];
-    int index = 1;
-    while(index < size)
-    {
-        if (array[index] < min) min = array[index];
-        else if (array[index] > max) max = array[index];
-        index++;
-    }
-    int diff = max - min;
-    Console.WriteLine(min + " " + max);
-}
-
-PrintarrayayDouble(array);
-Console.WriteLine();
-FindMaxMin(array);
-Console.WriteLine(max-min);
+double[] arr = CreateArrayRndDouble(5, 1 ,100);
+PrintArrayDouble(arr);
+double findMaxMin = FindMaxMin(arr);
+Console.WriteLine($"Difference between max and min element: = {findMaxMin:F1} ");
