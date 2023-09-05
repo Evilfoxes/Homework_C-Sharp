600cb4d3b1747de31ef0e3d32a196654f8cc7b13
evilfoxes <evilfoxes@mail.ru>
2023-09-05 19:37:09 +1100

    Formating_Task37

--- task37/Program.cs
+++ task37/Program.cs
@@ -42,25 +42,3 @@ int[] array = CreateArrayRndInt(4, 1, 9);
 PrintArray(array);
 int[] pairMultiplacation = PairMultiplacation(array);
 PrintArray(pairMultiplacation);
-
-// double[] CreateArrayRndDouble(int size, int min, int max)
-// {
-//     double[] arr = new double[size];
-//     Random rnd = new Random();
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         arr[i] = rnd.NextDouble() * (max - min) + min;
-//     }
-//     return arr;
-// }
-
-// void PrintArrayDouble(double[] arr)
-// {
-//     Console.Write("[");
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         if (i < arr.Length - 1) Console.Write($"{arr[i]},");
-//         else Console.Write($"{arr[i]:F1}");
-//     }
-//     Console.WriteLine("]");
-// }
