fab7f937c26a0077f98af709d92da59a916f24b9
evilfoxes <evilfoxes@mail.ru>
2023-09-07 20:52:53 +1100

    Save_Task40_02

--- task40/Program.cs
+++ task40/Program.cs
@@ -15,5 +15,5 @@ bool IsExistTriangle(int side1, int side2, int side3)
 {
     // if(side1 < side2 + side3 && side2 < side1 + side3 && side3 < side1 +side2) return true;
     // else return false;
-    return (side1 < side2 + side3 && side2 < side1 + side3 && side3 < side1 +side2);
+    return side1 < side2 + side3 && side2 < side1 + side3 && side3 < side1 +side2;
 }
