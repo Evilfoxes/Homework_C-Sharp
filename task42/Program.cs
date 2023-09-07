42c6415ce5f49b361cfb2502fd006aa81726bb13
evilfoxes <evilfoxes@mail.ru>
2023-09-07 21:24:49 +1100

    Save_Task42

---
+++ task42/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 46 -> 101110
+// 13 -> 1101
+// 2 -> 10
+
+Console.Write("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int DecimalToBin(int num)
+{
+    int res = 0;
+    int factor = 1;
+    while(num  > 0)
+    {
+        res = res + num % 2 * factor;
+        num /= 2;
+        factor *= 10;
+    }
+    return res;
+}
+
+int decimalToBin = DecimalToBin(number);
+Console.Write(decimalToBin);
