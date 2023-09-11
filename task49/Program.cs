25755b6810b147e1bfaf08792add4845f26ee228
evilfoxes <evilfoxes@mail.ru>
2023-09-11 16:50:12 +1100

    Save_Task49

--- task49/Program.cs
+++ task49/Program.cs
@@ -13,3 +13,47 @@
 // 1 4 7 2
 // 5 81 2 9
 // 8 4 2 4
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 6}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+void SearhEvenIndexRaplaceQuart(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)  
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2d);
+Console.WriteLine();
+SearhEvenIndexRaplaceQuart(array2d);
+PrintMatrix(array2d);
