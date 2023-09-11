1898c9830d6dddc38ad25e382472c582e6647b51
evilfoxes <evilfoxes@mail.ru>
2023-09-11 15:42:57 +1100

    Save_Task46

--- task46/Program.cs
+++ task46/Program.cs
@@ -35,12 +35,32 @@ void PrintMatrix(int[,] matrix)
 int[,] array2d = CreateMatrixRndInt(3, 4, -100, 100);
 PrintMatrix(array2d);
 
-// void ReverseArray(int[] arr)
-// {
-//     for (int i = 0; i < arr.Length / 2; i++)
-//     {
-//         int temp = arr[i];
-//         arr[i] = arr[arr.Length - 1 - i];
-//         arr[arr.Length - 1 - i] = temp;
-//     }
-// }
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 6}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -100, 100);
+PrintMatrix(array2d);
