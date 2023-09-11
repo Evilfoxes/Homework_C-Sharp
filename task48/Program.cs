a3669d61385c1360b948d6fd4ce8d828e1e02835
evilfoxes <evilfoxes@mail.ru>
2023-09-11 15:47:27 +1100

    Save_Task48

--- task48/Program.cs
+++ task48/Program.cs
@@ -5,3 +5,32 @@
 // 0 1 2 3
 // 1 2 3 4
 // 2 3 4 5
+
+int[,] CreateMatrixSumIndex(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 6}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+int[,] arraySumIndex = CreateMatrixSumIndex(3, 4);
+PrintMatrix(arraySumIndex);
