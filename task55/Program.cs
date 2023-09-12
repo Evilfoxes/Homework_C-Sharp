070880c7d8c6f78970489e7242d29e9a70db8b3a
evilfoxes <evilfoxes@mail.ru>
2023-09-12 21:48:23 +1100

    Save_Task55

--- task55/Program.cs
+++ task55/Program.cs
@@ -2,3 +2,51 @@
 // которая заменяет строки на столбцы. В случае, если это
 // невозможно, программа должна вывести сообщение для
 // пользователя.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+void ReplaceRowsColumns(int[,] matrix)
+{   
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < i)
+            {
+                int temp = matrix[i, j];
+                matrix[i, j] = matrix[j, i];
+                matrix[j, i] = temp;
+            }     
+        }
+    }    
+}
+int[,] createMatrixRndInt = CreateMatrixRndInt(3, 3, -100, 100);
+PrintMatrix(createMatrixRndInt);
+Console.WriteLine();
+ReplaceRowsColumns(createMatrixRndInt);
+PrintMatrix(createMatrixRndInt);
