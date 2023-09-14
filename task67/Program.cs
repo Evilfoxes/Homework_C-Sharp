cf99a9b2524e73ab401edbbd5f1889bc5ef56577
evilfoxes <evilfoxes@mail.ru>
2023-09-14 21:20:05 +1100

    Save_Task67

--- task67/Program.cs
+++ task67/Program.cs
@@ -3,3 +3,14 @@
 
 // 453 -> 12
 // 45 -> 9
+
+int SumDigitNumber(int num)
+{
+    if (num == 0) return 0;
+    else return num % 10 + SumDigitNumber(num / 10);
+}
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumDigitNumber(number));
