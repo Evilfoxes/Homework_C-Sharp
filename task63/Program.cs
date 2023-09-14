56a2d4a129c1164df5ec01b5851a4b7e98bf575a
evilfoxes <evilfoxes@mail.ru>
2023-09-14 20:21:10 +1100

    Creat_Task63

--- task63/Program.cs
+++ task63/Program.cs
@@ -3,3 +3,21 @@
 
 // N = 5 -> "1, 2, 3, 4, 5"
 // N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Write("Enter natural number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 1)
+{
+    Console.Write("Input Error!!!");
+    return;
+}
+
+NaturalNumbers(number);
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
