774a59fe894512ce1bf4951619434317b83e8135
evilfoxes <evilfoxes@mail.ru>
2023-09-15 22:23:06 +1100

    Save_Task66_03

--- task66/Program.cs
+++ task66/Program.cs
@@ -4,16 +4,16 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.Write("Enter number M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
 int SumNumbersFromMToN(int numberN, int numberM)
 {
     if (numberN > numberM) return 0;
     return SumNumbersFromMToN(numberN, numberM - 1) + numberM;
 }
-
-Console.Write("Enter number M: ");
-int numM = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter number N: ");
-int numN = Convert.ToInt32(Console.ReadLine());
 
 if (numN >= numM)
 {
